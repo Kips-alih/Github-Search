256a5644e764c1bbe82f66e1c48d08e7786e2a5f
Kipngeno <alphonce.kipngeno@student.moringaschool.com>
2021-10-17 23:39:29 +0300

    Imort components, add routes to home,form and about. Redirect empty paths to home and throw error to unexisting paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { FormComponent } from './form/form.component';
+import { GithubComponent } from './github/github.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+
+{ path: "home",component: GithubComponent},
+{ path: "form",component: FormComponent},
+{ path: 'about', component: AboutComponent},
+{ path: '', redirectTo:"home", pathMatch:"full"},
+{ path:'**', component:NotFoundComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
