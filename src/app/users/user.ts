75354e76c918ccb8cfac0e1b16906fd94ae7b646
Kipngeno <alphonce.kipngeno@student.moringaschool.com>
2021-10-17 23:02:22 +0300

    Create a user class

---
+++ src/app/users/user.ts
@@ -0,0 +1,9 @@
+export class User {
+  constructor(
+    public public_repos: number,
+    public login: string,
+    public avatar_url: string,
+    public created_at: Date,
+    public html_url:string
+) { }
+}
